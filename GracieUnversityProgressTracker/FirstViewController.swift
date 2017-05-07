a977631a800f62bbbf1be30ce68c68b2d70c761e
Adam Baca <baca.adam@me.com>
2017-05-07 17:58:18 -0600

    Final commit to master branch

--- GracieUnversityProgressTracker/FirstViewController.swift
+++ GracieUnversityProgressTracker/FirstViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
-    @IBOutlet weak var circleProgressView: CircleProgressView!
     let nf = NumberFormatter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         nf.numberStyle = NumberFormatter.Style.decimal
         nf.maximumFractionDigits = 2
+        
         
     }
 
